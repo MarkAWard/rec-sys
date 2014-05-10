a07e54a013264e50dd247f33d0ab03f1d11ea12d
Robert Moakler <rtm288@nyu.edu>
2014-05-10 01:12:36 -0400

    playing around with implicit, readme is not up to date yet.

--- src/collaborative_filtering_global.py
+++ src/collaborative_filtering_global.py
@@ -67,6 +67,15 @@ parser.add_option("-L", "--lambda4", action="store",
 parser.add_option("-t", "--training", action="store_false", 
                   dest="train", default=True, 
                   help="should training take place, or should we just grab the pretrained weights")
+parser.add_option("-c", "--continue", action="store_true", 
+                  dest="cont", default=False, 
+                  help="continue training with weight matrices that already exist?")
+parser.add_option("-f", "--fake_implicit", action="store_true", 
+                  dest="fake_implicit", default=False, 
+                  help="just use a binary R as implicit?")
+parser.add_option("-i", "--iterations", action="store", 
+                  dest="iterations", type="string", default=None, 
+                  help="number of iterations")
 
 def main():
     # Get command line options
@@ -76,6 +85,14 @@ def main():
     neighbor_hood_size = int(options.k)
     l4 = int(options.l4)
     train = options.train
+    cont = options.cont
+    fake_implicit = options.fake_implicit
+    n_iterations = int(options.iterations)
+
+    if fake_implicit:
+        faking = 'implicit-n'
+    else:
+        faking = 'implicit-y'
 
     # Parameters for the global model
     g = float(options.g)
@@ -83,10 +100,15 @@ def main():
 
     # Get the ratings matrix
     R = np.array(pickle.load(open(pickle_directory + prefix + '_stars_train.p')).todense())
-    
+    N = np.array(R, copy=True)
+    N[N.nonzero()] = 1
+
     # Dimensions
     m = R.shape[0]      # number of users
     n = R.shape[1]      # number of businesses
+
+    if neighbor_hood_size == -1:
+        neighbor_hood_size = n
 
     # Find all the businesses rated by each user ...
     print 'Counting and binning users ... ',
@@ -105,6 +127,51 @@ def main():
         users = np.where(R[:,i] > 0)[0]
         business_buckets[i] = users
     print 'Done'
+
+    # Get implicit ratings
+    print '\nCalculating implicit data matrix ... '
+    sys.stdout.flush()
+    i_to_h = pickle.load(open(pickle_directory + prefix + '_idx_to_user_hash.p'))
+    h_to_i = pickle.load(open(pickle_directory + prefix + '_user_hash_to_idx.p'))
+
+    with open(data_directory + 'yelp_academic_dataset_user.json') as file:
+        lc = 0
+        for line in file:
+            lc += 1
+
+    with open(data_directory + 'yelp_academic_dataset_user.json') as file:
+        i = 0
+        for line in file:
+            current = json.loads(line)
+            if current['user_id'] in h_to_i:
+                try:
+                    friends = current['friends']
+                    for f in friends:
+                        if f in h_to_i:
+                            N[h_to_i[current['user_id']],:] += R[h_to_i[f],:]
+                except:
+                    pass
+            if i % 10 == 0 or i == (lc - 1):
+                sys.stdout.write('\r' + str(round(float(i)/(lc - 1) * 100, 2)) + '%    ')
+                sys.stdout.flush()
+            i += 1
+
+    N[N.nonzero()] = 1
+    print 'Done'
+
+
+    # Bin implicit
+    print 'Binning implicit data ... ',
+    sys.stdout.flush()
+    user_implicit_buckets = {}
+    for i in range(0,N.shape[0]):
+        businesses = np.where(N[i,:] > 0)[0]
+        user_implicit_buckets[i] = businesses
+    print 'Done'
+
+    if fake_implicit:
+        N = np.array(R, copy=True)
+        user_implicit_buckets = user_buckets
 
     # Get the average of each column and row
     business_means = np.ma.average(R, axis=0, weights=R!=0).data
@@ -136,30 +203,42 @@ def main():
         b_u[u] = float(numerator) / (l3 + len(user_buckets[u]))
 
 
-    # Get all the matrices we'll need
+    # Get the Ratings
     R = pickle.load(open(pickle_directory + prefix + '_stars_train.p'))
 
     R_row = R.nonzero()[0]
     R_col = R.nonzero()[1]
 
+    print "\nTraining ..."
     if train:
-        P = pickle.load(open(pickle_directory + prefix + '_P_' + distance + '_k' + str(n) + '_l' + str(l4) + '.p'))
-        P = np.array(P.todense())
-        C = np.array(P, copy = True)
-    else:
-        try:
-            P = pickle.load(open(pickle_directory + prefix + '_W_k' + str(neighbor_hood_size) + '_l' + str(l5) + '_g' + str(g) + '.p'))
-            C = pickle.load(open(pickle_directory + prefix + '_C_k' + str(neighbor_hood_size) + '_l' + str(l5) + '_g' + str(g) + '.p'))
+        if cont:
+            P = pickle.load(open(pickle_directory + prefix + '_P_' + distance + '_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l4) + '.p'))
+            W = pickle.load(open(pickle_directory + prefix + '_W_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l5) + '_g' + str(g) + '.p'))
+            C = pickle.load(open(pickle_directory + prefix + '_C_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l5) + '_g' + str(g) + '.p'))
             P = np.array(P.todense())
             C = np.array(C.todense())
+            W = np.array(W.todense())
+        else:
+            P = pickle.load(open(pickle_directory + prefix + '_P_' + distance + '_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l4) + '.p'))
+            P = np.array(P.todense())
+            C = np.array(P, copy = True)
+            W = np.array(P, copy = True)
+    else:
+        try:
+            print pickle_directory + prefix + '_P_' + distance + '_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l4) + '.p'
+            P = pickle.load(open(pickle_directory + prefix + '_P_' + distance + '_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l4) + '.p'))
+            W = pickle.load(open(pickle_directory + prefix + '_W_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l5) + '_g' + str(g) + '.p'))
+            C = pickle.load(open(pickle_directory + prefix + '_C_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l5) + '_g' + str(g) + '.p'))
+            P = np.array(P.todense())
+            C = np.array(C.todense())
+            W = np.array(W.todense())
         except:
-            print "Those weights are not found. Are you sure you don't want to train?"
+            print "\nThose weights were not found. Are you sure you don't want to train?"
             sys.exit()
-
 
     if train:
         # Iterate and update the weights for explicit and implicit
-        for iteration in range(20):
+        for iteration in range(n_iterations):
             sse = 0
             sse_n = 0
 
@@ -171,18 +250,27 @@ def main():
                 sigma_r = 0
                 sigma_n = 0
 
-                intersection = np.intersect1d(np.where(P[i,:] != 0)[0], user_buckets[u])
+                intersection1 = np.intersect1d(np.where(P[i,:] != 0)[0], user_buckets[u])
                 
-                for j in intersection:
-                    sigma_r += (R[u, j] - (mu + b_i[j] + b_u[u])) * P[i, j]
+                for j in intersection1:
+                    sigma_r += (R[u, j] - (mu + b_i[j] + b_u[u])) * W[i, j]
+
+                intersection2 = np.intersect1d(np.where(P[i,:] != 0)[0], user_implicit_buckets[u])
+
+                for j in intersection2:
                     sigma_n += C[i, j]
 
-                if len(intersection) == 0:
+                if len(intersection1) == 0:
                     pow1 = 0
                 else:
-                    pow1 = np.power(len(intersection), -0.5)
+                    pow1 = np.power(len(intersection1), -0.5)
 
-                r_hat = mu + b_u[u] + b_i[i] + pow1 * sigma_r + pow1 * sigma_n
+                if len(intersection2) == 0:
+                    pow2 = 0
+                else:
+                    pow2 = np.power(len(intersection2), -0.5)
+
+                r_hat = mu + b_u[u] + b_i[i] + pow1 * sigma_r + pow2 * sigma_n
                 
                 err = R[u, i] - r_hat
                 sse += np.power(err, 2)
@@ -191,40 +279,44 @@ def main():
                 b_u[u] += g * (err - l5 * b_u[u])
                 b_i[i] += g * (err - l5 * b_i[i])
 
-                for j in intersection:
-                    P[i, j] += g * (np.power(len(intersection), -0.5) * err * (R[u, j] - (mu + b_u[u] + b_i[j])) - l5 * P[i, j])
-                    C[i, j] += g * (np.power(len(intersection), -0.5) * err - l5 * C[i, j])
+                for j in intersection1:
+                    W[i, j] += g * (np.power(len(intersection1), -0.5) * err * (R[u, j] - (mu + b_u[u] + b_i[j])) - l5 * W[i, j])
+
+                for j in intersection2:
+                    C[i, j] += g * (np.power(len(intersection2), -0.5) * err - l5 * C[i, j])
+                
                 if k % 10 == 0 or k == (len(R_row) - 1):
                     sys.stdout.write('\r' + str(round(float(k)/(len(R_row) - 1) * 100, 2)) + '%    ')
                     sys.stdout.flush()
-            print 'Done ... RMSE = ' + str(np.sqrt(sse/sse_n)) + '\n'
+            print '... RMSE = ' + str(np.sqrt(sse/sse_n)) + '\n'
 
 
     # Only keep k-most similar
     for i in range(n):
         modifier = np.zeros(n)
-        modifier[np.argsort(P[i,:])[::-1][:neighbor_hood_size]] = 1
-        P[i, :] = P[i, :] * modifier
+        modifier[np.argsort(W[i,:])[::-1][:neighbor_hood_size]] = 1
+        W[i, :] = W[i, :] * modifier
         C[i, :] = C[i, :] * modifier
 
     if train:
         # Store similarities
-        P = scipy.sparse.lil_matrix(P)
+        W = scipy.sparse.lil_matrix(W)
         C = scipy.sparse.lil_matrix(C)
-        pickle.dump(P, open(pickle_directory + prefix + '_W_k' + str(neighbor_hood_size) + '_l' + str(l5) + '_g' + str(g) + '.p', "wb"))
-        pickle.dump(C, open(pickle_directory + prefix + '_C_k' + str(neighbor_hood_size) + '_l' + str(l5) + '_g' + str(g) + '.p', "wb"))
-        print 'Stored W matrix into ' + pickle_directory + prefix + '_W_k' + str(neighbor_hood_size) + '_l' + str(l5) + '_g' + str(g) + '.p'
-        print 'Stored C matrix into ' + pickle_directory + prefix + '_C_k' + str(neighbor_hood_size) + '_l' + str(l5) + '_g' + str(g) + '.p'
-        P = np.array(P.todense())
+        pickle.dump(W, open(pickle_directory + prefix + '_W_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l5) + '_g' + str(g) + '.p', "wb"))
+        pickle.dump(C, open(pickle_directory + prefix + '_C_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l5) + '_g' + str(g) + '.p', "wb"))
+        print 'Stored W matrix into ' + pickle_directory + prefix + '_W_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l5) + '_g' + str(g) + '.p'
+        print 'Stored C matrix into ' + pickle_directory + prefix + '_C_k' + str(neighbor_hood_size) + '_' + faking + '_l' + str(l5) + '_g' + str(g) + '.p'
+        W = np.array(W.todense())
         C = np.array(C.todense())
 
     # Clean the weights
-    P[P < 0] = 0
+    W[W < 0] = 0
     C[C < 0] = 0
     print 'Done'
 
     # Get the validation matrix
-    V = pickle.load(open(pickle_directory + prefix + '_stars_validate.p')).tocsr()
+    V = pickle.load(open(pickle_directory + prefix + '_stars_validate.p'))
+    R = R.todense()
 
     # Which cells do we need to predict?
     required_row = V.nonzero()[0]
@@ -242,18 +334,27 @@ def main():
         sigma_r = 0
         sigma_n = 0
 
-        intersection = np.intersect1d(np.where(P[i,:] != 0)[0], user_buckets[u])
-        
-        for j in intersection:
-            sigma_r += (R[u, j] - (mu + b_i[j] + b_u[u])) * P[i, j]
+        intersection1 = np.intersect1d(np.where(P[i,:] != 0)[0], user_buckets[u])
+
+        for j in intersection1:
+            sigma_r += (R[u, j] - (mu + b_i[j] + b_u[u])) * W[i, j]
+
+        intersection2 = np.intersect1d(np.where(P[i,:] != 0)[0], user_implicit_buckets[u])
+
+        for j in intersection2:
             sigma_n += C[i, j]
 
-        if len(intersection) == 0:
+        if len(intersection1) == 0:
             pow1 = 0
         else:
-            pow1 = np.power(len(intersection), -0.5)
+            pow1 = np.power(len(intersection1), -0.5)
 
-        r_hat = mu + b_u[u] + b_i[i] + pow1 * sigma_r + pow1 * sigma_n
+        if len(intersection2) == 0:
+            pow2 = 0
+        else:
+            pow2 = np.power(len(intersection2), -0.5)
+
+        r_hat = mu + b_u[u] + b_i[i] + pow1 * sigma_r + pow2 * sigma_n
 
 
         # Insert the prediction
@@ -266,7 +367,8 @@ def main():
 
     # Get the RMSE
     R = scipy.sparse.csr_matrix(R)
-    print '\nRMSE for global): \t' + str(tools.rmse(R, V))
+
+    print '\nRMSE for global: \t' + str(tools.rmse(R, V))
 
 if __name__ == '__main__':
     main()
